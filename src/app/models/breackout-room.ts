5d607d53fcc8fdfd9673bb42ebc39c4920aa60cd
Alex Scigalszky <alexpablo.90@gmail.com>
2021-08-25 07:38:49 -0300

    create breackout room model and load oficial meets

---
+++ src/app/models/breackout-room.ts
@@ -0,0 +1,4 @@
+export class BreakoutRoom {
+  url: string;
+  name: string;
+}
--- src/app/services/breakout-rooms.service.ts
+++ src/app/services/breakout-rooms.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BreakoutRoom } from '../models/breackout-room';
+import { Nullable } from '../models/nullable';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BreakoutRoomsService {
-  rooms: string[] = [
-    'https://meet.google.com/cjx-stse-neb',
-    'https://meet.google.com/erh-dcbr-day',
-    'https://meet.google.com/mcg-vmtq-mzm',
-    'https://meet.google.com/stm-zcuk-woi',
+  rooms: Nullable<BreakoutRoom[]> = [
+    { url: 'https://meet.google.com/stw-zgec-zez', name: 'patio' },
+    { url: 'https://meet.google.com/knv-dvzy-sgg', name: 'cafetería' },
+    { url: 'https://meet.google.com/nnu-mihm-nfw', name: 'sala de reuniones' },
+    { url: 'https://meet.google.com/ykv-kgyz-toq', name: 'piso de abajo' },
+    { url: 'https://meet.google.com/jsb-teai-zfd', name: 'cocina' },
+    { url: 'https://meet.google.com/tqr-ersq-bwy', name: 'piso de arriba' },
   ];
 
-  getRandomLink(): string {
-    let urlRoom: string = undefined;
+  getRandomLink(): BreakoutRoom {
+    let urlRoom: BreakoutRoom = undefined;
     do {
       const index = Math.floor(Math.random() * 4);
       urlRoom = this.rooms[index];
