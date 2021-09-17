3fae974ced6300367b7fa578ca4806afd782d923
Alex Scigalszky <alexpablo.90@gmail.com>
2021-09-17 10:40:47 -0300

    aniversaries service

--- src/app/services/aniversaries.service.spec.ts
+++ src/app/services/aniversaries.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import { Partner } from '../models/partner';
 
 import { AniversariesService } from './aniversaries.service';
 
 describe('AniversariesService', () => {
   let service: AniversariesService;
+  const partners: Partner[] = [new Partner()];
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -15,8 +17,23 @@ describe('AniversariesService', () => {
   });
 
   it('should return a list of people', () => {
-    const peoples = service.getWhoHaveAnAniversary()
+    const peoples = service.getWhoHaveAnAniversary([]);
     expect(peoples).toBeTruthy();
     expect(peoples).toBeGreaterThanOrEqual(0);
+  });
+
+  it('should filter all new people ', () => {
+    const peoples = service.getWhoHaveAnAniversary(partners);
+    expect(peoples.length).toEqual(0);
+  });
+
+  it('should filter people with one year in company ', () => {
+    const partner = new Partner();
+    partner.id = '123456';
+    partner.whenWasHired = new Date();
+    partner.whenWasHired.setFullYear(partner.whenWasHired.getFullYear() - 1);
+    const peoples = service.getWhoHaveAnAniversary([partner, ...partners]);
+    expect(peoples.length).toBeGreaterThanOrEqual(1);
+    expect(peoples[0].id).toEqual(partner.id);
   });
 });
--- src/app/services/aniversaries.service.ts
+++ src/app/services/aniversaries.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Partner } from '../models/partner';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AniversariesService {
-  
-  getWhoHaveAnAniversary(): [] {
-    return [];
+  getWhoHaveAnAniversary(partners: Partner[]): Partner[] {
+    return partners.filter((x) => this.hasAniversaryInThisWeek(x.whenWasHired));
   }
-  
+
+  hasAniversaryInThisWeek(date: Date): boolean {
+    const today = new Date();
+    const diffDays = this.calculateDiffInDays(date, today);
+    const debt = diffDays % 365;
+    const years = diffDays / 365;
+    return 0 <= debt && debt <= 7 && years > 0;
+  }
+
+  calculateDiffInDays(first: Date, second: Date) {
+    return Math.floor(
+      (Date.UTC(second.getFullYear(), second.getMonth(), second.getDate()) -
+        Date.UTC(first.getFullYear(), first.getMonth(), first.getDate())) /
+        (1000 * 60 * 60 * 24),
+    );
+  }
 }
