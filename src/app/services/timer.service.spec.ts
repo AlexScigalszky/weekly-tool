b917c6c18acedfe4884f6bafc6a8e560f7b62224
Alex Scigalszky <alexpablo.90@gmail.com>
2021-10-01 11:12:48 -0300

    TimerServiceTest

--- src/app/services/timer.service.spec.ts
+++ src/app/services/timer.service.spec.ts
@@ -13,4 +13,43 @@ describe('TimerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be initialize', () => {
+    service.initTimer();
+
+    expect(service.remainingTime).toBeGreaterThan(0);
+    expect(service.time).toBeGreaterThan(0);
+    expect(service.runTimer).toBeFalse();
+    expect(service.hasStarted).toBeFalse();
+    expect(service.hasFinished).toBeFalse();
+  });
+  
+  it('should be start', () => {
+    service.runTimer = false;
+    service.startTimer();
+
+    expect(service.runTimer).toBeFalse();
+  });
+  
+  it('should be start running', () => {
+    service.runTimer = true;
+    service.startTimer();
+
+    expect(service.runTimer).toBeTrue();
+    expect(service.hasStarted).toBeTrue();
+  });
+  
+  it('should be pause', () => {
+    service.pauseTimer();
+
+    expect(service.runTimer).toBeFalse();
+  });
+  
+  it('should reset', () => {
+    service.resetTimer();
+
+    expect(service.remainingTime).toBeGreaterThan(0);
+    expect(service.time).toBeGreaterThan(0);
+    expect(service.hasFinished).toBeFalse();
+  });
 });
